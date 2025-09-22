becc43e412e2828b37b4d18735a3d037cd9d38a8
unknown <ami145388@gmail.com>
2025-09-22 23:40:58 +0500

    added: primary color setting functionality

--- components/settings/settings.js
+++ components/settings/settings.js
@@ -1,11 +1,9 @@
 import { USER_SETTINGS } from '../../constants/user-settings.constants.js';
 import { loadComponent } from '../../utils/loadComponent.js';
 import {
-	getUserSettings,
+	getUserSetting,
 	setUserSetting,
 } from '../../utils/userSettingsStore.js';
-
-let startingPageSelect;
 
 export async function initSettings(container) {
 	const settingsRoot = await loadComponent(
@@ -14,25 +12,47 @@ export async function initSettings(container) {
 		container
 	);
 
-	startingPageSelect = settingsRoot.querySelector(
-		'#settings-starting-page-select'
+	// cache elements
+	const startingPageSelect = settingsRoot.querySelector(
+		'#settings-starting-page'
 	);
+	const colorInput = settingsRoot.querySelector('#settings-primary-color');
+	const themeToggle = settingsRoot.querySelector('#settings-theme');
 
-	startingPageSelect.addEventListener('change', () => {
-		const currentValue = startingPageSelect?.value;
+	// ---------- INIT ----------
+	loadSavedSettings();
 
-		if (!currentValue) return;
-
-		setUserSetting(USER_SETTINGS.STARTING_PAGE, currentValue);
+	// ---------- EVENT LISTENERS ----------
+	startingPageSelect.addEventListener('change', e => {
+		setUserSetting(USER_SETTINGS.STARTING_PAGE, e.target.value);
 	});
 
-	// initialize state
-	loadSavedSettings();
-}
+	colorInput.addEventListener('input', e => {
+		const newColor = e.target.value;
+		document.documentElement.style.setProperty('--primary-color', newColor);
+		setUserSetting(USER_SETTINGS.PRIMARY_COLOR, newColor);
+	});
 
-function loadSavedSettings() {
-	const userSettings = getUserSettings();
-	console.log(userSettings);
+	// themeToggle.addEventListener('change', e => {
+	// 	const isDark = e.target.checked;
+	// 	document.documentElement.classList.toggle('dark', isDark);
+	// 	setUserSetting(USER_SETTINGS.THEME, isDark ? 'dark' : 'light');
+	// });
 
-	startingPageSelect.value = userSettings[USER_SETTINGS.STARTING_PAGE];
+	// ---------- HELPERS ----------
+	function loadSavedSettings() {
+		// starting page
+		const savedPage = getUserSetting(USER_SETTINGS.STARTING_PAGE, 'search');
+		startingPageSelect.value = savedPage;
+
+		// primary color
+		const savedColor = getUserSetting(USER_SETTINGS.PRIMARY_COLOR, '#ff5656');
+		document.documentElement.style.setProperty('--primary-color', savedColor);
+		colorInput.value = savedColor;
+
+		// theme
+		// const savedTheme = getUserSetting(USER_SETTINGS.THEME, 'light');
+		// document.documentElement.classList.toggle('dark', savedTheme === 'dark');
+		// themeToggle.checked = savedTheme === 'dark';
+	}
 }
